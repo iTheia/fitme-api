a5f042505d53a8a6c57acc9fdb74f2b854f2d6a6
Sirakusa <herrera.clip@gmail.com>
2024-06-24 20:48:03 -0600

    feat: add HTTP Health check

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { HealthCheckModule } from "./health-check/health-check.module";
+import { ConfigModule } from "@nestjs/config";
+import { config, configSchemaValidation } from "./config";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: config,
+      validationSchema: configSchemaValidation,
+    }),
+    HealthCheckModule,
+  ],
+})
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
-  const logger = new Logger('Main');
+  const logger = new Logger("Main");
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
+  const port = configService.get("PORT");
 
   await app.listen(port, () => {
     logger.log(`Service started on port ${port}`);
