0441e332515980d2afdc0e7cedb43d0c8280ab44
Sirakusa <herrera.clip@gmail.com>
2024-06-27 02:20:26 -0600

    feat: add configuration of Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('Main');
@@ -9,8 +10,19 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
+  const config = new DocumentBuilder()
+    .setTitle('fitme api')
+    .setDescription('Endpoints of fitme api')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port, () => {
     logger.log(`Service started on port ${port}`);
   });
+
+  logger.log(`Swagger http://localhost:3000/api`);
 }
 bootstrap();
