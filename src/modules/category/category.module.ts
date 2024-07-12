28a4166b52b17079ce91d69dee491ab0a2089064
Sirakusa <herrera.clip@gmail.com>
2024-07-11 23:51:13 -0600

    feat: add category module

---
+++ src/modules/category/category.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CategoryController } from './category.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from './store/category.entity';
+import { CategoryRepository } from './store/category.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { PaginationService } from '@modules/pagination/pagination.service';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('secretToken').secretToken,
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([
+      { name: Category.name, schema: CategorySchema },
+    ]),
+  ],
+  controllers: [CategoryController],
+  providers: [CategoryService, CategoryRepository, PaginationService],
+})
+export class CategoryModule {}
