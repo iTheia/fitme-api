dbf534079e2fedd67864711152c12938fd19af78
Sirakusa <herrera.clip@gmail.com>
2024-07-12 18:49:43 -0600

    fix: remove unnecessary paths and rename environment variable

--- src/config/images.ts
+++ src/config/images.ts
@@ -1,5 +1,5 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('multer', () => ({
-  dest: process.env.MULTER_DEST,
+export default registerAs('images', () => ({
+  dest: process.env.IMAGES_DEST,
 }));
