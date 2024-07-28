6d729438b80c228a29919d0b5dce4c1f882b3480
Marlon <martinez.ded@gmail.com>
2024-07-27 18:18:39 -0600

    Remove fuzzy search lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
 import { Module } from '@nestjs/common';
 import { HealthCheckModule } from './modules/health-check/health-check.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
 import { config, configSchemaValidation } from './config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { TokenModule } from './modules/token/token.module';
-import { NotificationModule } from './modules/notification/notification.module';
-import { CacheModule } from './modules/cache/cache.module';
-import { EventEmitterModule } from './modules/event-emitter/event-emitter.module';
 import { CategoryModule } from './modules/category/category.module';
 import { ImagesModule } from './modules/images/images.module';
 import { ExerciseModule } from './modules/exercise/exercise.module';
 import { RoutineModule } from './modules/routine/routine.module';
+import { NotificationModule } from '@modules/notification/notification.module';
+import { CacheModule } from '@modules/cache/cache.module';
+import { EventEmitterModule } from '@modules/event-emitter/event-emitter.module';
 
 @Module({
   imports: [
@@ -21,14 +22,21 @@ import { RoutineModule } from './modules/routine/routine.module';
       load: config,
       validationSchema: configSchemaValidation,
     }),
-    HealthCheckModule,
-    MongooseModule.forRootAsync({
+    BullModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        return { uri: configService.get('database').url };
-      },
+      useFactory: (configService: ConfigService) => ({
+        url: configService.get('redis').url,
+      }),
       inject: [ConfigService],
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('database').url,
+      }),
+      inject: [ConfigService],
+    }),
+    HealthCheckModule,
     AuthModule,
     UserModule,
     TokenModule,
